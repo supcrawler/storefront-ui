ede694009647d75610d5b9b5d5f463e17ea72de2
supcrawler <eosbornguy@gmail.com>
2019-03-27 11:55:23 -0300

    docs: remove css classes (minor, commented)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <div style="display: flex; ">
-      <SfBanner class="sf-banner--right">
-        <template slot="title">
-          ECO SANDALS
-        </template>
-        <template slot="subtitle">
-          Summer shoes
-        </template>
-        <template slot="description">
-          The collection features formal and casual comfort shoes with a Danish
-          design focus. Made from premium leathers and comfort.
-        </template>
-        <template slot="button">
-          shop now
-        </template>
-      </SfBanner>
-    </div>
+    <SfBanner>
+      <template slot="title">
+        ECO SANDALS
+      </template>
+      <template slot="subtitle">
+        Summer shoes
+      </template>
+      <template slot="description">
+        The collection features formal and casual comfort shoes with a Danish
+        design focus. Made from premium leathers and comfort.
+      </template>
+      <template slot="button">
+        shop now
+      </template>
+    </SfBanner>
   </div>
 </template>
 
