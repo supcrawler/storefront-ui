30c0895fde1db50993876018c86bee775e2216e1
supcrawler <eosbornguy@gmail.com>
2019-03-28 10:06:28 +0100

    Create working-with-alpaca.md

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <SfButton></SfButton>
+    <SfBanner>
+      <template slot="title">
+        ECO SANDALS
+      </template>
+      <template slot="subtitle">
+        Summer shoes
+      </template>
+      <template slot="description">
+        The collection features formal and casual comfort shoes with a Danish
+        design focus. Made from premium leathers and comfort.
+      </template>
+      <template slot="button">
+        shop now
+      </template>
+    </SfBanner>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import SfButton from "./components/atoms/SfButton/SfButton.vue";
+import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
 
 export default Vue.extend({
   name: "app",
   components: {
-    SfButton
+    SfBanner
   }
 });
 </script>
