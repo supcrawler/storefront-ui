d24cb622bd4858594a5542b9eea40268674ec6f1
supcrawler <eosbornguy@gmail.com>
2019-03-28 12:01:39 +0100

    updated banner and rules

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
   <div id="app">
-    <SfBanner>
-      <template slot="title">
-        ECO SANDALS
-      </template>
-      <template slot="subtitle">
-        Summer shoes
-      </template>
-      <template slot="description">
-        The collection features formal and casual comfort shoes with a Danish
-        design focus. Made from premium leathers and comfort.
-      </template>
-      <template slot="button">
-        shop now
-      </template>
-    </SfBanner>
+    Put examples here
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import SfButton from "./components/atoms/SfButton/SfButton.vue";
-import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
 
 export default Vue.extend({
   name: "app",
-  components: {
-    SfBanner
-  }
+  components: {}
 });
 </script>
 
