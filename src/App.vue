55ba87ab2d8528ac5c62df4fe714c16258b59d03
supcrawler <eosbornguy@gmail.com>
2019-03-29 13:33:42 +0100

    fixed typo in scss vars

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <SfBanner
+      title="Lorem ipsum"
+      subtitle="Lorem ipsum"
+      button-text="Lorem ipsum"
+    >
+      <template slot="call-to-action">
+        Example
+      </template>
+    </SfBanner>
+    <SfCircleButton></SfCircleButton>
+    <SfButton></SfButton>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
+import SfCircleButton from "./components/atoms/SfCircleButton/SfCircleButton.vue";
 
 export default Vue.extend({
-  name: "app"
+  name: "app",
+  components: {
+    SfCircleButton,
+    SfButton,
+    SfBanner
+  }
 });
 </script>
 
