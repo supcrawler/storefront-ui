75220c7b708a847e3c5abbb42bd9b1291c51e2b7
supcrawler <eosbornguy@gmail.com>
2019-04-03 22:21:33 +0700

    Using label tag as parent without dynamic component and updated unit test

--- src/components/template/SfComponent.spec.ts
+++ src/components/template/SfComponent.spec.ts
@@ -2,14 +2,15 @@ import { shallowMount } from "@vue/test-utils";
 import SfComponent from "@/components/template/SfComponent.vue";
 
 describe("SfComponent.vue", () => {
-  it("renders a component", () => {
-    const component = shallowMount(SfComponent);
-    expect(component.contains(".sf-component")).toBe(true);
-  });
+  // DOM Element render check, you can replace with with root component
+  // it("renders a button", () => {
+  //   const component = shallowMount(SfComponent);
+  //   expect(component.contains("button")).toBe(true);
+  // });
 
   // Default slot check, you can replace `default` with any other
-  // it("renders default prop text when passed", () => {
-  //   const component = shallowMount(SfComponent);
-  //   expect(component).toBeDefined();
-  // });
+  it("renders default prop text when passed", () => {
+    const component = shallowMount(SfComponent);
+    expect(component).toBeDefined();
+  });
 });
