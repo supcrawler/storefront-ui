7d4a145a992ce55fea391ba80c55ff295edb9631
supcrawler <eosbornguy@gmail.com>
2019-04-09 18:35:43 +0700

    Remove labelClass from props and add default slot

---
+++ src/components/molecules/SfOptions/SfOptions.js
@@ -0,0 +1,47 @@
+import SfLabel from "../../atoms/SfLabel/SfLabel.vue";
+
+export default {
+  name: "SfOptions",
+  components: {
+    SfLabel
+  },
+  model: {
+    prop: "selected",
+    event: "change"
+  },
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    label: {
+      type: String,
+      default: null
+    },
+    image: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: String,
+      default: null
+    },
+    optionClass: {
+      type: String,
+      default: null
+    },
+    wrapperClass: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    setActiveValue(value) {
+      this.$emit("change", value);
+    }
+  }
+};
