aea36374fb44450885c2fef90bfcfda68ec44510
supcrawler <eosbornguy@gmail.com>
2019-04-17 19:34:32 -0300

    fix(input-number): force sync with input and current value (prevent empty)

--- src/components/molecules/SfSidebar/SfSidebar.spec.ts
+++ src/components/molecules/SfSidebar/SfSidebar.spec.ts
@@ -1,9 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import SfSidebar from "./SfSidebar.vue";
+import SfComponent from "@/components/template/SfComponent.vue";
 
 describe("SfSidebar.vue", () => {
   it("renders a component", () => {
-    const component = shallowMount(SfSidebar);
+    const component = shallowMount(SfComponent);
     expect(component.contains(".sf-sidebar")).toBe(true);
   });
 });
