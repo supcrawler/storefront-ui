c753834943737555e223fcef4f7af6f4f90ee3fe
supcrawler <eosbornguy@gmail.com>
2019-04-29 10:52:37 +0200

    product card improvements and simplification

--- src/components/molecules/SfMenuItem/SfMenuItem.spec.ts
+++ src/components/molecules/SfMenuItem/SfMenuItem.spec.ts
@@ -9,14 +9,14 @@ describe("SfMenuItem.vue", () => {
 
   it("renders correct count prop", () => {
     const countText = "10 item(s)";
-    const labelText = "Example title";
+    const titleText = "Example title";
     const component = shallowMount(SfMenuItem, {
       propsData: {
-        label: labelText,
+        title: titleText,
         count: countText
       }
     });
-    expect(component.find(".sf-menu-item__label").text()).toMatch(labelText);
+    expect(component.find(".sf-menu-item__title").text()).toMatch(titleText);
     expect(component.find(".sf-menu-item__count").text()).toMatch(countText);
   });
 
