467a44ee46c845d63cce1757cdbcc2032a682878
supcrawler <eosbornguy@gmail.com>
2019-05-29 12:43:24 +0200

    Added moved per peage not per item

---
+++ src/components/organisms/SfCarousel/SfCarousel.js
@@ -0,0 +1,62 @@
+import Vue from "vue";
+
+import SfCarouselItem from "@/components/organisms/SfCarousel/_internal/SfCarouselItem.vue";
+import SfArrow from "@/components/atoms/SfArrow/SfArrow.vue";
+
+import Glide from "@glidejs/glide";
+
+Vue.component("SfCarouselItem", SfCarouselItem);
+
+export default {
+  name: "SfCarousel",
+  data() {
+    return {
+      glide: null
+    };
+  },
+  props: {
+    perPage: {
+      type: Number,
+      default: 4
+    }
+  },
+  components: {
+    SfArrow
+  },
+  methods: {
+    go(direct) {
+      switch (direct) {
+        case "<":
+          this.glide.go("<");
+          break;
+        case ">":
+          this.glide.go(">");
+          break;
+      }
+    }
+  },
+  mounted: function() {
+    const glide = new Glide(this.$refs.glide, {
+      type: "carousel",
+      perView: this.perPage,
+      peek: 0,
+      breakpoints: {
+        768: {
+          perView: 2,
+          peek: {
+            before: 0,
+            after: 100
+          }
+        }
+      }
+    });
+    glide.mount();
+    glide.on("run.before", move => {
+      const { perView } = glide.settings;
+      const { direction } = move.direction;
+
+      move.steps = direction === "<" ? perView : -perView;
+    });
+    this.glide = glide;
+  }
+};
---
+++ src/components/organisms/SfCarousel/SfCarousel.stories.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, text, select } from "@storybook/addon-knobs";
+import notes from "./SfCarousel.md";
+import SfCarousel from "./SfCarousel.vue";
+import SfProductCard from "../../molecules/SfProductCard/SfProductCard.vue";
+
+storiesOf("Organisms|[WIP] Carousel", module)
+  .addDecorator(withKnobs)
+  .add(
+    "Basic",
+    () => ({
+      components: { SfCarousel, SfProductCard },
+      data() {
+        return {
+          products: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+        };
+      },
+      template: `
+      <div style="max-width: 1140px">
+        <SfCarousel :per-page="4">
+          <SfCarouselItem v-for="(product, key) in products">
+            <SfProductCard
+              :title="'Product ' + (key + 1)"
+              :price="{ regularPrice: '10,99 $'}"
+              :rating="{ max: 5, score: 4 }"/>
+          </SfCarouselItem>
+        </SfCarousel>
+      </div>
+      `
+    }),
+    {
+      info: true,
+      notes
+    }
+  );
