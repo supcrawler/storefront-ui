2b68303245da9521dc0f1d3b57aff594e5b3301d
supcrawler <eosbornguy@gmail.com>
2019-06-03 10:55:33 +0200

    Change static  values to scss var

--- src/components/molecules/SfProductOption/SfProductOption.stories.js
+++ src/components/molecules/SfProductOption/SfProductOption.stories.js
@@ -1,34 +1,8 @@
 import { storiesOf } from "@storybook/vue";
-import SfProductOption from "./SfProductOption.vue";
-import { generateStorybookTable } from "@/helpers";
-import { withKnobs, text, select } from "@storybook/addon-knobs";
 
-const scssTableConfig = {
-  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
-  tableBodyConfig: [
-    [
-      "$product-option-padding",
-      "$spacing-big $spacing-extra-big",
-      "padding for product option"
-    ],
-    [
-      "$product-optionicon-spacing",
-      "0.75rem",
-      "spacing for product option icon"
-    ],
-    ["$product-option-min-height", "35px", "minimum height for product option"],
-    [
-      "$product-option-font-size",
-      "$font-size-big-mobile",
-      "font-size for product option"
-    ],
-    [
-      "$product-option-font-family",
-      "$body-font-family-secondary",
-      "font-family height for product option"
-    ]
-  ]
-};
+import SfProductOption from "./SfProductOption.vue";
+
+import { withKnobs, text, select } from "@storybook/addon-knobs";
 
 export default storiesOf("Molecules|ProductOption", module)
   .addDecorator(withKnobs)
@@ -58,7 +32,6 @@ export default storiesOf("Molecules|ProductOption", module)
         summary: `
           <h2> Usage </h2>
           <pre><code>import SfProductOption from "@storefrontui/vue/dist/SfProductOption.vue"</code></pre>
-          ${generateStorybookTable(scssTableConfig, "SCSS variables")}
           `
       }
     }
--- src/components/molecules/SfSelect/SfSelect.stories.js
+++ src/components/molecules/SfSelect/SfSelect.stories.js
@@ -18,20 +18,11 @@ storiesOf("Molecules|Select", module)
         };
       },
       components: { SfSelect, SfProductOption },
-      props: {
-        customClass: {
-          default: select(
-            "CSS Modifier",
-            ["null", "sf-select--bordered", "sf-select--underlined"],
-            "null"
-          )
-        }
-      },
       template: `
       <div>
         <p><b>Selected: {{selected}}</b></p>
-        <div style="max-width: 509px">
-          <SfSelect v-model="selected" :class="customClass">
+        <div style="width: 300px">
+          <SfSelect v-model="selected">
             <SfSelectOption value="amaranth">
               <SfProductOption color="#E52B50" label="Amaranth"/>
             </SfSelectOption>
