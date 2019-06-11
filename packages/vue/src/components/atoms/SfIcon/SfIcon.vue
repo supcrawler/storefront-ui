76756345f62a0d0424b5f169f295e7bf23baab32
supcrawler <eosbornguy@gmail.com>
2019-06-11 12:11:25 +0200

    fix css paths

--- packages/vue/src/components/atoms/SfIcon/SfIcon.vue
+++
@@ -1,5 +0,0 @@
-<script src="./SfIcon.js"></script>
-<template lang="html" src="./SfIcon.html"></template>
-<style lang="scss">
-@import "~@storefrontui/shared/styles/components/SfIcon.scss";
-</style>
