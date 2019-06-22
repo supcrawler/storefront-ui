3cbfb5c680e648b701973075b2327c1687bfdc52
supcrawler <eosbornguy@gmail.com>
2019-06-22 15:51:44 +0200

    ref. #94 replace emit close with toggle

--- packages/vue/src/components/organisms/SfTabs/SfTabs.vue
+++
@@ -1,5 +0,0 @@
-<script src="./SfTabs.js"></script>
-<template lang="html" src="./SfTabs.html"></template>
-<style lang="scss">
-@import "~@storefrontui/shared/styles/components/SfTabs.scss";
-</style>
