98e14cf11058e147a409c8724c138af6c94b7f56
supcrawler <eosbornguy@gmail.com>
2019-06-26 09:45:03 +0200

    Adds a story to SfBullet component

---
+++ packages/vue/src/components/organisms/SfHero/SfHero.js
@@ -0,0 +1,86 @@
+import Vue from "vue";
+
+import SfHeroItem from "./_internal/SfHeroItem.vue";
+import SfArrow from "../../atoms/SfArrow/SfArrow.vue";
+import SfBullets from "../../atoms/SfBullets/SfBullets.vue";
+
+import Glide from "@glidejs/glide";
+
+Vue.component("SfHeroItem", SfHeroItem);
+
+export default {
+  name: "SfHero",
+  data() {
+    return {
+      glide: null,
+      defaultOptions: {
+        type: "slider",
+        rewind: true,
+        autoplay: 2000,
+        perView: 1
+      }
+    };
+  },
+  props: {
+    /** Carousel options like glide.js (https://glidejs.com/docs/) */
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+    heroItems: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    SfArrow,
+    SfBullets
+  },
+  methods: {
+    go(direct) {
+      console.log(direct);
+      switch (direct) {
+        case "prev":
+          this.glide.go("<");
+          break;
+        case "next":
+          this.glide.go(">");
+          break;
+        default:
+          this.glide.go(`=${direct}`);
+          break;
+      }
+    }
+  },
+  computed: {
+    mergedOptions() {
+      return {
+        ...this.defaultOptions,
+        ...this.options
+      };
+    },
+    numberOfPages() {
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!!this.$slots.default) {
+        return (
+          this.$slots.default.filter(slot => slot.tag).length +
+          this.heroItems.length
+        );
+      } else {
+        return this.heroItems.length;
+      }
+    },
+    page() {
+      if (this.glide) {
+        return this.glide.index + 1;
+      }
+      return 1;
+    }
+  },
+  mounted: function() {
+    const glide = new Glide(this.$refs.glide, this.mergedOptions);
+    glide.mount();
+
+    this.glide = glide;
+  }
+};
