e8898be1a97f6f0afd511eb5225b8e708af2f95d
supcrawler <eosbornguy@gmail.com>
2019-07-04 09:45:00 +0200

    Improves Accordion Item

---
+++ packages/vue/src/components/organisms/SfAccordion/_internal/SfAccordionItem.js
@@ -0,0 +1,23 @@
+export default {
+  name: "SfAccordionItem",
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  props: {
+    headerText: {
+      type: String,
+      default: ""
+    },
+    contentItems: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    onHeaderClick() {
+      this.$parent.$emit("toggle", this._uid);
+    }
+  }
+};
