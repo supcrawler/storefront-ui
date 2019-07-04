5eeaf8aec87aee4737e9b5dcd7473f429b500382
supcrawler <eosbornguy@gmail.com>
2019-07-04 09:46:07 +0200

    Added SfCharacteristic component

--- packages/vue/src/components/organisms/SfAccordion/SfAccordion.js
+++ packages/vue/src/components/organisms/SfAccordion/SfAccordion.js
@@ -4,44 +4,5 @@ import SfAccordionItem from "./_internal/SfAccordionItem.vue";
 Vue.component("SfAccordionItem", SfAccordionItem);
 
 export default {
-  name: "SfAccordion",
-  props: {
-    /**
-     * Allows to open multiple accordion items if set to "true"
-     */
-    multiple: {
-      type: Boolean,
-      default: false
-    },
-    /**
-     * Opens the first accordion item if set to "true"
-     */
-    firstOpen: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    toggle(slotId) {
-      if (!this.multiple) {
-        this.$children.forEach(child => {
-          child._uid === slotId
-            ? (child.isOpen = !child.isOpen)
-            : (child.isOpen = false);
-        });
-      } else {
-        const clickedHeader = this.$children.find(child => {
-          return child._uid === slotId;
-        });
-        clickedHeader.isOpen = !clickedHeader.isOpen;
-      }
-    },
-    openFirst() {
-      this.firstOpen ? (this.$children[0].isOpen = true) : "";
-    }
-  },
-  mounted: function() {
-    this.$on("toggle", this.toggle);
-    this.openFirst();
-  }
+  name: "SfAccordion"
 };
