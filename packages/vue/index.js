b0592c57cbb5fd873b35b561f8ba731da9970d5c
supcrawler <eosbornguy@gmail.com>
2019-07-11 14:49:10 +0000

    Bump lodash.template from 4.4.0 to 4.5.0

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -2,6 +2,7 @@
 import SfArrow from "./src/components/atoms/SfArrow/SfArrow.vue";
 import SfBadge from "./src/components/atoms/SfBadge/SfBadge.vue";
 import SfBreadcrumbs from "./src/components/atoms/SfBreadcrumbs/SfBreadcrumbs.vue";
+import SfBullets from "./src/components/atoms/SfBullets/SfBullets.vue";
 import SfButton from "./src/components/atoms/SfButton/SfButton.vue";
 import SfCheckbox from "./src/components/atoms/SfCheckbox/SfCheckbox.vue";
 import SfCircleIcon from "./src/components/atoms/SfCircleIcon/SfCircleIcon.vue";
@@ -16,7 +17,6 @@ import SfRating from "./src/components/atoms/SfRating/SfRating.vue";
 import SfAlert from "./src/components/molecules/SfAlert/SfAlert.vue";
 import SfBanner from "./src/components/molecules/SfBanner/SfBanner.vue";
 import SfCallToAction from "./src/components/molecules/SfCallToAction/SfCallToAction.vue";
-import SfCharacteristic from "./src/components/molecules/SfCharacteristic/SfCharacteristic.vue";
 import SfCounter from "./src/components/molecules/SfCounter/SfCounter.vue";
 import SfFilter from "./src/components/molecules/SfFilter/SfFilter.vue";
 import SfGallery from "./src/components/molecules/SfGallery/SfGallery.vue";
@@ -36,10 +36,12 @@ import SfList from "./src/components/organisms/SfList/SfList.vue";
 import SfMegaMenu from "./src/components/organisms/SfMegaMenu/SfMegaMenu.vue";
 import SfSidebar from "./src/components/organisms/SfSidebar/SfSidebar.vue";
 import SfTopBar from "./src/components/organisms/SfTopBar/SfTopBar.vue";
+import SfHero from "./src/components/organisms/SfHero/SfHero.vue";
 
 export {
   SfArrow,
   SfBadge,
+  SfBullets,
   SfButton,
   SfBreadcrumbs,
   SfCheckbox,
@@ -54,7 +56,6 @@ export {
   SfAlert,
   SfBanner,
   SfCallToAction,
-  SfCharacteristic,
   SfCounter,
   SfFilter,
   SfGallery,
@@ -72,5 +73,6 @@ export {
   SfCarousel,
   SfList,
   SfMegaMenu,
-  SfTopBar
+  SfTopBar,
+  SfHero
 };
