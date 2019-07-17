3cb6f09111defd360b29abf0b1eea22d105f11cb
supcrawler <eosbornguy@gmail.com>
2019-07-17 13:00:54 +0200

    fix for docs

--- packages/vue/src/examples/pages/home/Home.stories.js
+++ packages/vue/src/examples/pages/home/Home.stories.js
@@ -10,13 +10,8 @@ storiesOf("Examples|Pages|Home", module)
     "Home",
     () => ({
       components: { Home },
-      props: {
-        bannerGrid: {
-          default: select("Banner Grid", [1, 2, 3, 4], 3)
-        }
-      },
       template: `
-      <Home :bannerGrid="bannerGrid"/>
+      <Home />
     `
     }),
     {
