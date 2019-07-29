33cf0c28c8c764c067adfb21e4f021267eca1de5
supcrawler <eosbornguy@gmail.com>
2019-07-29 17:26:44 +0200

    Reaplace old tab to new one, fix input import of tranistions.scss

---
+++ packages/vue/src/components/organisms/SfTabs/_internal/SfTab.js
@@ -0,0 +1,28 @@
+import {
+  Fragment
+} from 'vue-fragment'
+
+import SfChevron from '../../../atoms/SfChevron/SfChevron.vue'
+
+export default {
+  name: 'Sftab',
+  components: {
+    Fragment,
+    SfChevron
+  },
+  props: {
+    tab: {
+      type: Object,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    tabClick() {
+      this.$emit('tabClick', this.index)
+    }
+  },
+}
