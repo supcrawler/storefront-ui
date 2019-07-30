d71daac60c2a0aec3da771009d129756431249e5
supcrawler <eosbornguy@gmail.com>
2019-07-30 14:50:13 +0200

    change update to change

--- packages/vue/src/components/organisms/SfTabs/SfTabs.js
+++ packages/vue/src/components/organisms/SfTabs/SfTabs.js
@@ -1,46 +1,74 @@
-import SfTab from "./_internal/SfTab.vue";
+import Vue from "vue";
+import SfTab, { TAB_COMPONENT_NAME } from "./_internal/SfTab.vue";
+import SfTabsNavigation from "./_internal/SfTabsNavigation.vue";
+
+Vue.component("SfTab", SfTab);
+Vue.component("SfTabsNavigation", SfTabsNavigation);
 
 export default {
   name: "SfTabs",
-  components: {
-    SfTab
+
+  provide() {
+    return {
+      changeTab: this.changeTab,
+      sharedState: this.sharedState,
+      fireEvent: this.fireEvent,
+      listenEvent: this.listenEvent
+    };
+  },
+
+  model: {
+    prop: "value",
+    event: "change"
   },
 
   props: {
-    /** Array of tabs objects  */
-    tabs: {
-      type: Array,
-      default: [
-        {
-          title: "Apple",
-          content: "Apple is a great fruit",
-          isActive: false
-        },
-        {
-          title: "Banana",
-          content: "Banana is a great fruit",
-          isActive: false
-        },
-        {
-          title: "Peach",
-          content: "Peach is a great fruit",
-          isActive: false
-        },
-        {
-          title: "Orange",
-          content: "Orange is a great fruit",
-          isActive: false
-        }
-      ]
+    /**
+     * Value of selected option, this prop has v-model support
+     */
+    value: {
+      type: [Number, String],
+      default: null
     }
   },
 
+  data() {
+    return {
+      sharedState: {
+        activeTabValue: this.value,
+        tabs: []
+      }
+    };
+  },
+
+  mounted() {
+    this.setTabs();
+    this.$nextTick(() => {
+      if (!this.value) {
+        this.changeTab(this.sharedState.tabs[0].valueComputed);
+      }
+    });
+  },
+
+  updated() {
+    this.setTabs();
+  },
+
   methods: {
-    activate(index) {
-      this.tabs.forEach((tab, tabIndex) => {
-        tab.isActive = false;
-        if (tabIndex === index) tab.isActive = true;
+    changeTab(tab) {
+      this.$set(this.sharedState, "activeTabValue", tab);
+      this.fireEvent("change", tab);
+    },
+    setTabs() {
+      this.sharedState.tabs = this.$children.filter(component => {
+        return component.$options.name === TAB_COMPONENT_NAME;
       });
+    },
+    fireEvent(name, value) {
+      this.$emit(name, value);
+    },
+    listenEvent(name, callback) {
+      this.$on(name, callback);
     }
   }
 };
