3c0c8779ddda5a555d8b868154caa72b710ecda8
supcrawler <eosbornguy@gmail.com>
2019-09-03 12:42:31 +0200

    Added mobile card feature POC

---
+++ packages/vue/src/components/molecules/SfAddToCart/SfAddToCart.js
@@ -0,0 +1,51 @@
+import SfButton from "../../atoms/SfButton/SfButton.vue";
+import SfInput from "../../atoms/SfInput/SfInput.vue";
+import { toInt } from "@glidejs/glide/src/utils/unit";
+
+export default {
+  name: "SfAddToCart",
+  components: {
+    SfButton,
+    SfInput
+  },
+  model: {
+    prop: "qty"
+  },
+  props: {
+    /**
+     * Boolean to indicate whether product
+     * can be added to cart
+     */
+    canAddToCart: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Stock quantity of product
+     */
+    stock: {
+      type: Number,
+      default: 1
+    },
+    /**
+     * Selected quantity
+     */
+    qty: {
+      type: String,
+      default: "1"
+    }
+  },
+  watch: {
+    qty(value) {
+      const qty = toInt(value);
+      if (qty <= 0) {
+        this.$emit("input", "1");
+        return;
+      }
+      if (qty > this.stock) {
+        this.$emit("input", "" + this.stock);
+        return;
+      }
+    }
+  }
+};
