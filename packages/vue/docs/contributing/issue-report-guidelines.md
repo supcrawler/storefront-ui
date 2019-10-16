8026f74bd65e29f5e61ed30341c579f0f48839a4
supcrawler <eosbornguy@gmail.com>
2019-10-16 13:27:22 +0200

    cleared SfCircleIcon

--- packages/vue/docs/contributing/issue-report-guidelines.md
+++ packages/vue/docs/contributing/issue-report-guidelines.md
@@ -1,18 +1,5 @@
 # How to report an issue
 
-## Issue types
-
-For organizing and tracking purpose, issues on StorefrontUI are divided into the main following types:
-
-* Bug report
-* Feature request (RFC)
-* Design issue
-
 ## Issue template
-### Bug report
 
-### Feature request (RFC)
-
-### Design related
-
-### Others
+TBD
--- packages/vue/src/components/atoms/SfArrow/SfArrow.stories.js
+++ packages/vue/src/components/atoms/SfArrow/SfArrow.stories.js
@@ -21,22 +21,24 @@ const scssTableConfig = {
     [
       "$arrow__button-shadow--right",
       "0 -5px 12px -5px rgba($c-dark-primary, 0.6)",
-      "shadow for hovered right arrow"
+      "shadow for hovered right arrow button"
     ],
     ["$arrow__icon-color", "$c-white", "color for icon"],
-    ["$arrow__icon-color--hover", "$c-dark-primary", "color for hovered icon"]
+    ["$arrow__icon-color--hover", "$c-dark-primary", "color for hovered"]
   ]
 };
 
 const cssTableConfig = {
   tableHeadConfig: ["NAME", "DESCRIPTION"],
   tableBodyConfig: [
-    [".sf-arrow--long", "change short to long arrow"],
-    [".sf-arrow--right", "transform left to right arrow"]
+    ["sf-arrow--right", "transform left to right arrow"],
+    ["sf-arrow--long", "change short to long arrow"],
+    ["sf-arrow--transparent", "removed background from button"],
+    ["sf-arrow--rounded", "changed to circle button"],
+    ["sf-arrow--no-shadow", "removed shadow from hovered button"]
   ]
 };
 
-const style = {};
 storiesOf("Atoms|Arrow", module)
   .addDecorator(withKnobs)
   .add(
@@ -66,8 +68,7 @@ storiesOf("Atoms|Arrow", module)
         summary: `<h2> Usage </h2>
         <pre><code>import { SfArrow } from "@storefront-ui/vue"</code></pre>
         ${generateStorybookTable(scssTableConfig, "SCSS variables")}
-        ${generateStorybookTable(cssTableConfig, "CSS modifiers")}
-        `
+        ${generateStorybookTable(cssTableConfig, "CSS modifiers")}`
       }
     }
   );
