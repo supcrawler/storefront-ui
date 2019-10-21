e4a66454dafac2f8d33bb676e5899a48f7defe14
supcrawler <eosbornguy@gmail.com>
2019-10-21 10:04:55 +0200

    fixed SfImage stories alt props

--- packages/vue/src/components/organisms/SfHeader/SfHeader.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.js
@@ -1,16 +1,3 @@
-const DEFAULT_LOGO = "/assets/logo.svg";
-
 export default {
-  name: "SfHeader",
-  props: {
-    logo: {
-      type: String,
-      default: DEFAULT_LOGO
-    }
-  },
-  computed: {
-    isDefaultLogo() {
-      return this.logo === DEFAULT_LOGO;
-    }
-  }
+  name: "SfHeader"
 };
