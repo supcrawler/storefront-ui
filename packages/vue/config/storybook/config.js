1936c72f2669364dc936027c903b30cfb5b63727
supcrawler <eosbornguy@gmail.com>
2019-10-21 10:41:47 +0200

    set lf eol for *.md

--- packages/vue/config/storybook/config.js
+++ packages/vue/config/storybook/config.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure, addDecorator, addParameters } from "@storybook/vue";
 import { withA11y } from "@storybook/addon-a11y";
+import { setDefaults, withInfo } from "storybook-addon-vue-info";
 
 import theme from "./theme";
 
@@ -8,6 +9,7 @@ import "../../src/css/_all.scss";
 
 const req = require.context("../../src/", true, /.stories.js$/);
 
+addDecorator(withInfo);
 addDecorator(withA11y);
 
 addParameters({
@@ -18,6 +20,10 @@ addParameters({
     theme,
     sortStoriesByKind: true
   }
+});
+setDefaults({
+  summary: `- Check **Knobs** tab on Props story to play with compomnent props and CSS modifiers story to play with CSS modifiers.
+  - Check **Notes** tab for documentation`
 });
 
 function loadStories() {
--- packages/vue/src/components/atoms/SfButton/SfButton.stories.js
+++ packages/vue/src/components/atoms/SfButton/SfButton.stories.js
@@ -1,41 +1,86 @@
 // /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/vue";
-import {
-  withKnobs,
-  text,
-  optionsKnob as options
-} from "@storybook/addon-knobs";
+import { withKnobs, optionsKnob as options } from "@storybook/addon-knobs";
+import { generateStorybookTable } from "@/helpers";
 
 import SfButton from "./SfButton.vue";
 
+// use this to document scss vars
+const scssTableConfig = {
+  tableHeadConfig: ["NAME", "DEFAULT", "DESCRIPTION"],
+  tableBodyConfig: [
+    ["$button-color", "$c-text-on-dark", "Default button text color"],
+    [
+      "$button-background-color",
+      "$c-green-primary",
+      "Default button background color"
+    ],
+    ["$button-border-radius", "0", "Button border radius"],
+    ["$button-text-transform", "uppercase", "Transform button text"],
+    [
+      "$button-desktop-padding",
+      "0.9375rem 2.5rem",
+      "Button spacing on desktop screens"
+    ],
+    [
+      "$button-desktop-font-size",
+      "1.125rem",
+      "Button font size on desktop screens"
+    ],
+    [
+      "$button-mobile-padding",
+      "0.5rem 1.7rem",
+      "Button spacing on mobile screens"
+    ],
+    [
+      "$button-mobile-font-size",
+      "0.75rem",
+      "Button font size on mobile screens"
+    ]
+  ]
+};
+
 storiesOf("Atoms|Button", module)
   .addDecorator(withKnobs)
-  .add("[slot] default", () => ({
-    props: {
-      customClass: {
-        default: options(
-          "CSS modifier",
-          {
-            "sf-button--outline": "sf-button--outline",
-            "sf-button--full-width": "sf-button--full-width",
-            "color-primary": "color-primary",
-            "color-secondary": "color-secondary",
-            "color-warning": "color-warning",
-            "color-danger": "color-danger",
-            "color-info": "color-info",
-            "color-success": "color-success"
-          },
-          "",
-          { display: "multi-select" }
-        )
+  .add(
+    "Basic",
+    () => ({
+      components: { SfButton },
+      props: {
+        customClass: {
+          default: options(
+            "CSS Modifiers",
+            {
+              "color-primary": "color-primary",
+              "color-secondary": "color-secondary",
+              "color-warning": "color-warning",
+              "color-danger": "color-danger",
+              "color-info": "color-info",
+              "color-success": "color-success",
+              "sf-button--full-width": "sf-button--full-width",
+              "sf-button--outline": "sf-button--outline"
+            },
+            "",
+            { display: "multi-select" }
+          )
+        }
       },
-      customLabel: {
-        default: text("default (slot)", "Shop now")
+      template: `<SfButton :class="customClass">Hello world</SfButton>`
+    }),
+    {
+      info: {
+        summary: `
+        <h2>
+          Description
+        </h2>
+        <p>
+          Basic button component,
+          called by <code>&lt;SfButton&gt;</code>.
+        </p>
+        <h2>Usage</h2>
+        <pre><code>import { SfButton } from "@storefront-ui/vue"</code></pre>
+        ${generateStorybookTable(scssTableConfig, "SCSS variables")}
+        `
       }
-    },
-    components: { SfButton },
-    template: `<SfButton
-      :class="customClass">
-      {{customLabel}}
-    </SfButton>`
-  }));
+    }
+  );
