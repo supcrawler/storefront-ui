b1c02f5e2bcd18252245f21493801f0b8e475455
supcrawler <eosbornguy@gmail.com>
2019-11-07 13:55:05 +0100

    fix variable name

---
+++ packages/vue/src/components/organisms/SfGroupedProduct/_internal/SfGroupedProductItem.js
@@ -0,0 +1,105 @@
+import SfPrice from "../../../atoms/SfPrice/SfPrice.vue";
+import SfImage from "../../../atoms/SfImage/SfImage.vue";
+import SfInput from "../../../atoms/SfInput/SfInput.vue";
+
+export default {
+  name: "SfGroupedProductItem",
+  components: {
+    SfImage,
+    SfPrice,
+    SfInput
+  },
+  inject: ["provided"],
+  props: {
+    /**
+     * Product image
+     */
+    image: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Product image placeholder
+     */
+    placeholderImage: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Product image lazy loading
+     */
+    lazyImage: {
+      type: Boolean,
+      default: true
+    },
+    /**
+     * Product image picture breakpoint
+     */
+    pictureBreakpointImage: {
+      type: Number,
+      default: 576
+    },
+    /**
+     * Product title
+     */
+    title: {
+      type: String,
+      required: true
+    },
+    /**
+     * Product sku
+     */
+    sku: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Product regular price
+     */
+    regularPrice: {
+      type: [Number, String],
+      default: ""
+    },
+    /**
+     * Product special price
+     */
+    specialPrice: {
+      type: [Number, String],
+      default: ""
+    },
+    /** Product quantity */
+    qty: {
+      type: String,
+      default: "0"
+    },
+    /**
+     * Stock quantity of product
+     */
+    stock: {
+      type: Number,
+      default: 0
+    },
+    /**
+     * Product attributes
+     */
+    attributes: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    qtyHandler(qty) {
+      const product = {
+        image: this.image,
+        title: this.title,
+        sku: this.sku,
+        specialPrice: this.specialPrice,
+        regularPrice: this.regularPrice,
+        qty: qty,
+        stock: this.stock,
+        attributes: this.attributes
+      };
+      this.provided.inputHandler(product);
+    }
+  }
+};
