f82fc0503b497cf14a5f8abe345e29904ee8d306
supcrawler <eosbornguy@gmail.com>
2019-11-13 11:14:02 +0100

    Added mobile slot with logo

--- packages/vue/src/components/organisms/SfHeader/SfHeader.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.js
@@ -1,3 +1,35 @@
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+
 export default {
-  name: "SfHeader"
+  name: "SfHeader",
+  components: {
+    SfIcon
+  },
+  props: {
+    logo: {
+      type: String,
+      default: ""
+    },
+    navigations: {
+      type: Array,
+      default: () => []
+    },
+    cartIcon: {
+      type: String,
+      default: "empty_cart"
+    },
+    wishlistIcon: {
+      type: String,
+      default: "heart"
+    },
+    accountIcon: {
+      type: String,
+      default: "profile"
+    }
+  },
+  methods: {
+    onClickHandler(name) {
+      this.$emit(`click:${name}`);
+    }
+  }
 };
