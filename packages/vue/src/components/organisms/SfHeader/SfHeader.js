1efbeaaf7a3e9c620fbdab1c7166daa5c3d16277
supcrawler <eosbornguy@gmail.com>
2019-11-13 10:22:27 +0100

    fix z-index

--- packages/vue/src/components/organisms/SfHeader/SfHeader.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.js
@@ -1,35 +1,3 @@
-import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
-
 export default {
-  name: "SfHeader",
-  components: {
-    SfIcon
-  },
-  props: {
-    logo: {
-      type: String,
-      default: "assets/logo.svg"
-    },
-    navigations: {
-      type: Array,
-      default: () => []
-    },
-    cartIcon: {
-      type: String,
-      default: "empty_cart"
-    },
-    wishlistIcon: {
-      type: String,
-      default: "heart"
-    },
-    accountIcon: {
-      type: String,
-      default: "profile"
-    }
-  },
-  methods: {
-    onClickHandler(name) {
-      this.$emit(`click:${name}`);
-    }
-  }
+  name: "SfHeader"
 };
