26ece1d5a5faa7f4f76781b5f6d272e1aa177b47
supcrawler <eosbornguy@gmail.com>
2019-11-18 20:14:10 +0100

    delete yarn.lock

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -1,129 +1,24 @@
-import { storiesOf } from "@storybook/vue";
-import { withKnobs, text } from "@storybook/addon-knobs";
-
-import SfHeader from "./SfHeader.vue";
-import SfHeaderNavItem from "./_internal/SfHeaderNavItem.vue";
-
-storiesOf("Organisms/Header", module)
-  .addDecorator(withKnobs)
-  .add("Default", () => ({
-    components: { SfHeader, SfHeaderNavItem },
-    props: {
-      logo: {
-        default: text("(prop) logo", "/assets/logo.svg")
-      },
-      cartIcon: {
-        default: text("(prop) cartIcon", "empty_cart")
-      },
-      wishlistIcon: {
-        default: text("(prop) wishlistIcon", "heart")
-      },
-      accountIcon: {
-        default: text("(prop) accountIcon", "profile")
-      }
-    },
-    template: `<SfHeader 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon">
-        <template #navigation>
-          <SfHeaderNavItem>WOMEN</SfHeaderNavItem>
-          <SfHeaderNavItem>MEN</SfHeaderNavItem>
-          <SfHeaderNavItem>KIDS</SfHeaderNavItem>
-        </template>
-      </SfHeader>`
-  }))
-  .add("[slot] logo", () => ({
-    components: { SfHeader, SfHeaderNavItem },
-    props: {
-      logo: {
-        default: text("(prop) logo", "/assets/heart.svg")
-      },
-      cartIcon: {
-        default: text("(prop) cartIcon", "empty_cart")
-      },
-      wishlistIcon: {
-        default: text("(prop) wishlistIcon", "heart")
-      },
-      accountIcon: {
-        default: text("(prop) accountIcon", "profile")
-      }
-    },
-    template: `<SfHeader 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon">
-      <template #logo>
-        CUSTOM LOGO 
-      </template>
-      <template #navigation>
-        <SfHeaderNavItem>WOMEN</SfHeaderNavItem>
-        <SfHeaderNavItem>MEN</SfHeaderNavItem>
-        <SfHeaderNavItem>KIDS</SfHeaderNavItem>
-      </template>
-      </SfHeader>`
-  }))
-  .add("[slot] search", () => ({
-    components: { SfHeader, SfHeaderNavItem },
-    props: {
-      logo: {
-        default: text("(prop) logo", "/assets/logo.svg")
-      },
-      cartIcon: {
-        default: text("(prop) cartIcon", "empty_cart")
-      },
-      wishlistIcon: {
-        default: text("(prop) wishlistIcon", "heart")
-      },
-      accountIcon: {
-        default: text("(prop) accountIcon", "profile")
-      }
-    },
-    template: `<SfHeader 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon">
-      <template #navigation>
-        <SfHeaderNavItem>WOMEN</SfHeaderNavItem>
-        <SfHeaderNavItem>MEN</SfHeaderNavItem>
-        <SfHeaderNavItem>KIDS</SfHeaderNavItem>
-      </template>
-      <template #search>
-        <img src="assets/storybook/doge.svg" style="height: 25px; margin-left: auto;" />
-      </template>
-      </SfHeader>`
-  }))
-  .add("[slot] icons", () => ({
-    components: { SfHeader, SfHeaderNavItem },
-    props: {
-      logo: {
-        default: text("(prop) logo", "/assets/logo.svg")
-      },
-      cartIcon: {
-        default: text("(prop) cartIcon", "empty_cart")
-      },
-      wishlistIcon: {
-        default: text("(prop) wishlistIcon", "heart")
-      },
-      accountIcon: {
-        default: text("(prop) accountIcon", "mail")
-      }
-    },
-    template: `<SfHeader 
-      :logo="logo"
-      :cart-icon="cartIcon" 
-      :wishlist-icon="wishlistIcon" 
-      :account-icon="accountIcon">
-      <template #navigation>
-        <SfHeaderNavItem>WOMEN</SfHeaderNavItem>
-        <SfHeaderNavItem>MEN</SfHeaderNavItem>
-        <SfHeaderNavItem>KIDS</SfHeaderNavItem>
-      </template>
-      <template #header-icons>
-        CUSTOM ICONS
-      </template>
-      </SfHeader>`
-  }));
+// import { storiesOf } from "@storybook/vue";
+// import notes from "./storybook-notes.md";
+// import SfHeader from "./SfHeader.vue";
+//
+// Work in progress
+// export default storiesOf("Organisms/Header", module)
+//   .add("mobile with icons", () => ({
+//     components: { SfHeader },
+//     template: `
+//     <sf-header>
+//       <img src="arrow_left.svg" alt="arrow" slot="left"/>
+//       <img src="arrow_right.svg" alt="arrow" slot="right"/>
+//     </sf-header>`,
+//     methods: {}
+//   }))
+//   .add("mobile with text", () => ({
+//     components: { SfHeader },
+//     template: `
+//     <sf-header>
+//       <img src="arrow_left.svg" alt="arrow" slot="left"/>
+//       <template slot="middle">Women</template>
+//     </sf-header>`,
+//     methods: {}
+//   }));
