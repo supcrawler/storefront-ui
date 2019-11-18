aa21736921fd6039a9db2f50a62d19199ac27eaf
supcrawler <eosbornguy@gmail.com>
2019-11-18 11:58:05 +0100

    Update packages/vue/src/components/organisms/SfHeader/SfHeader.md

--- packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.stories.js
@@ -1,24 +1,194 @@
-// import { storiesOf } from "@storybook/vue";
-// import notes from "./storybook-notes.md";
-// import SfHeader from "./SfHeader.vue";
-//
-// Work in progress
-// export default storiesOf("Organisms/Header", module)
-//   .add("mobile with icons", () => ({
-//     components: { SfHeader },
-//     template: `
-//     <sf-header>
-//       <img src="arrow_left.svg" alt="arrow" slot="left"/>
-//       <img src="arrow_right.svg" alt="arrow" slot="right"/>
-//     </sf-header>`,
-//     methods: {}
-//   }))
-//   .add("mobile with text", () => ({
-//     components: { SfHeader },
-//     template: `
-//     <sf-header>
-//       <img src="arrow_left.svg" alt="arrow" slot="left"/>
-//       <template slot="middle">Women</template>
-//     </sf-header>`,
-//     methods: {}
-//   }));
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, text } from "@storybook/addon-knobs";
+
+import SfHeader from "./SfHeader.vue";
+
+storiesOf("Organisms/Header", module)
+  .addDecorator(withKnobs)
+  .add("Default", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "profile")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+        <template #navigation>
+        <ul style="display: flex; margin: 0; padding: 0; list-style: none;">
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">WOMEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">MEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">KIDS</a>
+        </li>
+      </ul>
+        </template>
+      </SfHeader>`
+  }))
+  .add("[slot] logo", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/heart.svg")
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "profile")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigations="navigations"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      <template #navigation>
+        <ul style="display: flex; margin: 0; padding: 0; list-style: none;">
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">WOMEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">MEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">KIDS</a>
+        </li>
+      </ul>
+      </template>
+      </SfHeader>`
+  }))
+  .add("[slot] navigation", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "profile")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigation="navigation"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      <template #navigation>
+        <ul style="display: flex; margin: 0; padding: 0; list-style: none;">
+        <li>
+          <a style="color: hsl(136, 53%, 59%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">SHOES</a>
+        </li>
+        <li>
+          <a style="color: hsl(205, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">ACCESSORIES</a>
+        </li>
+      </ul>
+      </template>
+      </SfHeader>`
+  }))
+  .add("[slot] search", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "profile")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigations="navigations"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      <template #navigation>
+        <ul style="display: flex; margin: 0; padding: 0; list-style: none;">
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">WOMEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">MEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">KIDS</a>
+        </li>
+      </ul>
+      </template>
+      <template #search>
+        <img src="assets/storybook/doge.svg" style="height: 25px; margin-left: auto;" />
+      </template>
+      </SfHeader>`
+  }))
+  .add("[slot] icons", () => ({
+    components: { SfHeader },
+    props: {
+      logo: {
+        default: text("(prop) logo", "/assets/logo.svg")
+      },
+      cartIcon: {
+        default: text("(prop) cartIcon", "empty_cart")
+      },
+      wishlistIcon: {
+        default: text("(prop) wishlistIcon", "heart")
+      },
+      accountIcon: {
+        default: text("(prop) accountIcon", "mail")
+      }
+    },
+    template: `<SfHeader 
+      :logo="logo"
+      :navigations="navigations"
+      :cart-icon="cartIcon" 
+      :wishlist-icon="wishlistIcon" 
+      :account-icon="accountIcon">
+      <template #navigation>
+        <ul style="display: flex; margin: 0; padding: 0; list-style: none;">
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">WOMEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">MEN</a>
+        </li>
+        <li>
+          <a style="color: hsl(216, 8%, 12%); font-size: 0.875rem; font-weight: 500; padding-left: 40px;">KIDS</a>
+        </li>
+      </ul>
+      </template>
+      <template #icons>
+        CUSTOM ICONS
+      </template>
+      </SfHeader>`
+  }));
