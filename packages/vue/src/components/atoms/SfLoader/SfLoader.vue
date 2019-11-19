7a53cb19d46089e794779f3635b1046560e91c52
supcrawler <eosbornguy@gmail.com>
2019-11-19 11:53:02 +0100

    add transition

--- packages/vue/src/components/atoms/SfLoader/SfLoader.vue
+++ packages/vue/src/components/atoms/SfLoader/SfLoader.vue
@@ -1,5 +1,6 @@
 <script src="./SfLoader.js"></script>
 <template lang="html" src="./SfLoader.html"></template>
 <style lang="scss">
+@import "../../../utilities/transitions/transitions.scss";
 @import "~@storefront-ui/shared/styles/components/SfLoader.scss";
 </style>
