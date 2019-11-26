f5d27e9aceaf09dc5711e60712a4a90504f59b3b
supcrawler <eosbornguy@gmail.com>
2019-11-26 14:15:57 +0100

    Update packages/vue/src/components/organisms/SfHeader/_internal/SfHeaderNavItem.html

--- packages/vue/src/components/organisms/SfHeader/SfHeader.js
+++ packages/vue/src/components/organisms/SfHeader/SfHeader.js
@@ -1,3 +1,53 @@
+import SfIcon from "../../atoms/SfIcon/SfIcon.vue";
+import SfImage from "../../atoms/SfImage/SfImage.vue";
+import SfSearchBar from "../../molecules/SfSearchBar/SfSearchBar.vue";
+
 export default {
-  name: "SfHeader"
+  name: "SfHeader",
+  components: {
+    SfIcon,
+    SfImage,
+    SfSearchBar
+  },
+  props: {
+    /**
+     * Header logo path
+     */
+    logo: {
+      type: [String, Object],
+      default: ""
+    },
+    /**
+     * Header title
+     */
+    title: {
+      type: String,
+      default: ""
+    },
+    /**
+     * Header cartIcon (accepts same value as SfIcon)
+     */
+    cartIcon: {
+      type: [String, Boolean],
+      default: "empty_cart"
+    },
+    /**
+     * Header wishlistIcon (accepts same value as SfIcon)
+     */
+    wishlistIcon: {
+      type: [String, Boolean],
+      default: "heart"
+    },
+    /**
+     * Header accountIcon (accepts same value as SfIcon)
+     */
+    accountIcon: {
+      type: [String, Boolean],
+      default: "profile"
+    },
+    activeIcon: {
+      type: String,
+      default: ""
+    }
+  }
 };
