e474167acf7f5ea8fd9edf124f7c8285aa125bca
supcrawler <eosbornguy@gmail.com>
2019-12-04 19:15:55 +0100

    fix transitions

--- packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.js
+++ packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.js
@@ -12,22 +12,15 @@ export default {
     },
     /**
      */
-    colorIcon: {
+    iconColor: {
       type: String,
       default: "white"
     },
     /**
      */
-    sizeIcon: {
+    iconSize: {
       type: String,
-      default: "100%"
-    },
-    /**
-     * Sets aria-label for button icon
-     */
-    alt: {
-      type: String,
-      required: true
+      default: "20px"
     }
   },
   components: {
--- packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.stories.js
+++ packages/vue/src/components/atoms/SfCircleIcon/SfCircleIcon.stories.js
@@ -1,73 +1,92 @@
 // /* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from '@storybook/vue';
+import { storiesOf } from "@storybook/vue";
 import {
   withKnobs,
+  text,
   select,
+  color,
   optionsKnob as options
-} from '@storybook/addon-knobs';
-import { icons } from '@storefront-ui/shared/icons/icons';
-
-import SfCircleIcon from './SfCircleIcon.vue';
-
+} from "@storybook/addon-knobs";
+import { icons } from "@storefront-ui/shared/icons/icons";
 const iconsNames = Object.keys(icons);
 
-storiesOf('Atoms|CircleIcon', module)
+const StoriesPlaceholder = {
+  template: `<span style="font-size: 1.5rem; width: 20px; height: 20px; line-height: 0; display: flex; justify-content: center; align-items: center">1</span>`
+};
+
+import SfCircleIcon from "./SfCircleIcon.vue";
+
+storiesOf("Atoms|CircleIcon", module)
   .addDecorator(withKnobs)
-  .add('Default', () => ({
+  .add("Default", () => ({
     components: { SfCircleIcon },
     props: {
       customClass: {
         default: options(
-          'CSS modifier',
+          "CSS modifier",
           {
-            'sf-circle-icon--small': 'sf-circle-icon--small',
-            'sf-circle-icon--big': 'sf-circle-icon--big',
-            'color-primary': 'color-primary',
-            'color-secondary': 'color-secondary',
-            'color-warning': 'color-warning',
-            'color-danger': 'color-danger',
-            'color-info': 'color-info',
-            'color-success': 'color-success'
+            "color-primary": "color-primary",
+            "color-secondary": "color-secondary",
+            "color-warning": "color-warning",
+            "color-danger": "color-danger",
+            "color-info": "color-info",
+            "color-success": "color-success"
           },
-          '',
-          { display: 'multi-select' }
+          "",
+          { display: "multi-select" }
         )
       },
       icon: {
-        default: select('(prop) icon', iconsNames, 'home')
+        default: select("icon (prop)", iconsNames, "home")
+      },
+      iconSize: {
+        default: text("icon-size (prop)", "20px")
+      },
+      iconColor: {
+        default: color("icon-color (prop)", "#FFFFFF")
       }
     },
     template: `<SfCircleIcon
         :class="customClass"
-        :icon="icon" />`
+        :icon="icon" 
+        :icon-size="iconSize"
+        :icon-color="iconColor"
+     />`
   }))
-  .add('[slot] default', () => ({
-    components: { SfCircleIcon },
+  .add("[slot] default", () => ({
+    components: { SfCircleIcon, StoriesPlaceholder },
     props: {
       customClass: {
         default: options(
-          'CSS modifier',
+          "CSS modifier",
           {
-            'sf-circle-icon--small': 'sf-circle-icon--small',
-            'sf-circle-icon--big': 'sf-circle-icon--big',
-            'color-primary': 'color-primary',
-            'color-secondary': 'color-secondary',
-            'color-warning': 'color-warning',
-            'color-danger': 'color-danger',
-            'color-info': 'color-info',
-            'color-success': 'color-success'
+            "color-primary": "color-primary",
+            "color-secondary": "color-secondary",
+            "color-warning": "color-warning",
+            "color-danger": "color-danger",
+            "color-info": "color-info",
+            "color-success": "color-success"
           },
-          '',
-          { display: 'multi-select' }
+          "",
+          { display: "multi-select" }
         )
       },
       icon: {
-        default: select('icon (prop)', iconsNames, 'home')
+        default: select("icon (prop)", iconsNames, "home")
+      },
+      iconSize: {
+        default: text("icon-size (prop)", "20px")
+      },
+      iconColor: {
+        default: text("icon-color (prop)", "#FFFFFF")
       }
     },
     template: `<SfCircleIcon
       :class="customClass"
-      :icon="icon">
-      <span style="font-size: 1.5rem">1</span>
+      :icon="icon"
+      :icon-size="iconSize"
+      :icon-color="iconColor"
+    >
+      <StoriesPlaceholder/>
     </SfCircleIcon>`
   }));
