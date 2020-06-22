3124337a34af888e7ca5fa9065244660f2bb1571
supcrawler <eosbornguy@gmail.com>
2020-06-22 12:07:36 +0200

    feat: cr fixes

--- packages/vue/src/components/atoms/SfButton/SfButton.vue
+++ packages/vue/src/components/atoms/SfButton/SfButton.vue
@@ -1,25 +1,19 @@
 <template>
-  <component
-    :is="tag"
+  <button
     v-focus
     class="sf-button"
     v-bind="$attrs"
     :disabled="disabled"
-    :link="link"
     v-on="$listeners"
   >
     <!--@slot Use this slot to place content inside the button.-->
     <slot />
-  </component>
+  </button>
 </template>
 <script>
 import { focus } from "../../../utilities/directives";
-import SfLink from "../SfLink/SfLink.vue";
 export default {
   name: "SfButton",
-  components: {
-    SfLink,
-  },
   directives: {
     focus,
   },
@@ -30,18 +24,6 @@ export default {
     disabled: {
       type: Boolean,
       default: false,
-    },
-    /**
-     * Link for "a" tag, when empty it is button.
-     */
-    link: {
-      type: [String, Object],
-      default: "",
-    },
-  },
-  computed: {
-    tag() {
-      return this.link ? "SfLink" : "button";
     },
   },
 };
