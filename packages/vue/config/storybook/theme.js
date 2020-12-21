dad2943a625f2e062f07e80daa882d8505280aef
supcrawler <eosbornguy@gmail.com>
2020-12-21 12:50:23 +0100

    fix: separate arrows without pointer events

--- packages/vue/config/storybook/theme.js
+++ packages/vue/config/storybook/theme.js
@@ -1,4 +1,4 @@
-import { create } from "@storybook/theming/create";
+import { create } from "@storybook/theming";
 
 export default create({
   base: "light",
@@ -28,5 +28,5 @@ export default create({
 
   brandTitle: "StorefrontUI",
   brandUrl: "https://github.com/Divanteltd/storefront-ui",
-  brandImage: "../../public/assets/storybook/logo.png",
+  brandImage: "assets/storybook/logo.png"
 });
