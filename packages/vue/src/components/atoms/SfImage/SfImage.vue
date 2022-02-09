a07bd42dd1e6528976c0dd6ac7d49d046ee9ee3a
supcrawler <eosbornguy@gmail.com>
2022-02-09 14:12:08 +0530

    feat: change the docs tab to the first place in toolbar (#2246)

--- packages/vue/src/components/atoms/SfImage/SfImage.vue
+++ packages/vue/src/components/atoms/SfImage/SfImage.vue
@@ -110,7 +110,6 @@ export default {
       return arr;
     },
     srcset() {
-      if (this.sortedSrcsets.length === 0) return null;
       return this.sortedSrcsets.reduce(
         (str, set) =>
           `${this.prefix(str)}${set.src} ${this.srcsetDescriptor(set)}`,
