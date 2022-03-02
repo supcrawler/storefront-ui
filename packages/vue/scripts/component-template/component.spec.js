27529b1b02039e0d5a702fcbdd6008f100b84535
supcrawler <eosbornguy@gmail.com>
2022-03-02 12:00:34 -0300

    Update README.md

--- packages/vue/scripts/component-template/component.spec.js
+++ packages/vue/scripts/component-template/component.spec.js
@@ -4,6 +4,6 @@ import ComponentNameCamelCase from "./ComponentNameCamelCase.vue";
 describe("ComponentNameCamelCase.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(ComponentNameCamelCase);
-    expect(component.find(".ComponentNameKebabCase").exists()).toBe(true);
+    expect(component.contains(".ComponentNameKebabCase")).toBe(true);
   });
 });
