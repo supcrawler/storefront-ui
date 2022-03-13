fa032331711368f89f1500e34d24898daa297773
supcrawler <eosbornguy@gmail.com>
2022-03-13 19:51:42 +0530

    docs: added example to use playground and update command

--- packages/nuxt/layouts/default.vue
+++ packages/nuxt/layouts/default.vue
@@ -25,76 +25,74 @@
             :label="item.name"
           />
       </template>
-    </SfHeader>
-    <div>
-      <nuxt />
-      <SfFooter :column="4" multiple id="footer">
-        <SfFooterColumn title="About us">
-          <SfList>
-            <SfListItem
-              v-for="item in footer.aboutUs"
-              :key="item"
-              >
-              <SfMenuItem
-                :data-cy="`app-foter-url_about-us_${item.split(' ').join('-').toLowerCase()}`"
-                :label="item"
-              />
-            </SfListItem>
-          </SfList>
-        </SfFooterColumn>
-        <SfFooterColumn title="Departments">
-          <SfList>
-            <SfListItem
-              v-for="item in footer.departments"
-              :key="item"
+    </SfHeader>    
+    <nuxt />
+    <SfFooter :column="4" multiple id="footer">
+      <SfFooterColumn title="About us">
+        <SfList>
+          <SfListItem
+            v-for="item in footer.aboutUs"
+            :key="item"
             >
-              <SfMenuItem
-                :data-cy="`app-foter-url_departments_${item.split(' ').join('-').toLowerCase()}`"
-                :label="item"
-              />
-            </SfListItem>
-          </SfList>
-        </SfFooterColumn>
-        <SfFooterColumn title="Help">
-          <SfList>
-            <SfListItem
-              v-for="item in footer.help"
-              :key="item"
-            >
-              <SfMenuItem
-                :data-cy="`app-foter-url_help_${item.split(' ').join('-').toLowerCase()}`"
-                :label="item"
-              />
-            </SfListItem>
-          </SfList>
-        </SfFooterColumn>
-        <SfFooterColumn title="Payment & Delivery">
-          <SfList>
-            <SfListItem
-              v-for="item in footer.paymentsDelivery"
-              :key="item"
-            >
-              <SfMenuItem
-                :data-cy="`app-foter-url_payment_${item.split(' ').join('-').toLowerCase()}`"
-                :label="item"
-              />
-            </SfListItem>
-          </SfList>
-        </SfFooterColumn>
-        <SfFooterColumn title="Social">
-          <div class="footer__socials">
-            <SfImage 
-              v-for="item in footer.social" 
-              :key="item" :src="'/assets/'+item+'.svg'" 
-              :width="32" 
-              :height="32" 
-              :alt="`${item}-logo`"
-              class="footer__social-image" 
+            <SfMenuItem
+              :data-cy="`app-foter-url_about-us_${item.split(' ').join('-').toLowerCase()}`"
+              :label="item"
             />
-          </div>
-        </SfFooterColumn>
-      </SfFooter>
-    </div>
+          </SfListItem>
+        </SfList>
+      </SfFooterColumn>
+      <SfFooterColumn title="Departments">
+        <SfList>
+          <SfListItem
+            v-for="item in footer.departments"
+            :key="item"
+          >
+            <SfMenuItem
+              :data-cy="`app-foter-url_departments_${item.split(' ').join('-').toLowerCase()}`"
+              :label="item"
+            />
+          </SfListItem>
+        </SfList>
+      </SfFooterColumn>
+      <SfFooterColumn title="Help">
+        <SfList>
+          <SfListItem
+            v-for="item in footer.help"
+            :key="item"
+          >
+            <SfMenuItem
+              :data-cy="`app-foter-url_help_${item.split(' ').join('-').toLowerCase()}`"
+              :label="item"
+            />
+          </SfListItem>
+        </SfList>
+      </SfFooterColumn>
+      <SfFooterColumn title="Payment & Delivery">
+        <SfList>
+          <SfListItem
+            v-for="item in footer.paymentsDelivery"
+            :key="item"
+          >
+            <SfMenuItem
+              :data-cy="`app-foter-url_payment_${item.split(' ').join('-').toLowerCase()}`"
+              :label="item"
+            />
+          </SfListItem>
+        </SfList>
+      </SfFooterColumn>
+      <SfFooterColumn title="Social">
+        <div class="footer__socials">
+          <SfImage 
+            v-for="item in footer.social" 
+            :key="item" :src="'/assets/'+item+'.svg'" 
+            :width="32" 
+            :height="32" 
+            :alt="`${item}-logo`"
+            class="footer__social-image" 
+          />
+        </div>
+      </SfFooterColumn>
+    </SfFooter>
     <SfBottomNavigation class="smartphone-only">
       <SfBottomNavigationItem
           v-for="(item, key) in items"
